fa3ab5d45ab48f772ade27fbdcdf8d47ce758771
“[Matti <[matti.tamminen@codecontrol.fi]>
2021-09-15 15:03:01 +0300

    initial

---
+++ storeBack/src/db.ts
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
+
+dotenv.config()
+
+const {
+	POSTGRES_USER,
+	POSTGRES_PASSWORD,
+	POSTGRES_HOST,
+	POSTGRES_DB,
+	POSTGRES_DB_TEST,
+	PORT,
+	ENV
+} = process.env
+
+let client: Pool
+
+if (ENV?.toString().trim() == 'test') { // testing
+	client = new Pool({
+		host: POSTGRES_HOST,
+		database: POSTGRES_DB_TEST,
+		user: POSTGRES_USER,
+		password: POSTGRES_PASSWORD,
+		port: PORT as number | undefined
+	})
+	console.log('Test DB-connection started')
+} else if (ENV?.toString().trim() == 'dev') { // development
+	client = new Pool({
+		host: POSTGRES_HOST,
+		database: POSTGRES_DB,
+		user: POSTGRES_USER,
+		password: POSTGRES_PASSWORD,
+		port: PORT as number | undefined
+	})
+	console.log('Development DB-connection started')
+} else { // production
+	client = new Pool({
+		host: POSTGRES_HOST,
+		database: POSTGRES_DB,
+		user: POSTGRES_USER,
+		password: POSTGRES_PASSWORD,
+		port: PORT as number | undefined
+	})
+	console.log('Production DB-connection started')
+}
+
+export default client
---
+++ storeBack/src/server.ts
@@ -0,0 +1,38 @@
+import express, { Response, Request } from 'express'
+// import cors, { CorsOptions } from 'cors'
+// import { productRoutes } from './handlers/productsHandler'
+// import { customerRoutes } from './handlers/customersHandler'
+// import { userRoutes } from './handlers/usersHandler'
+// import { orderRoutes } from './handlers/ordersHandler'
+
+// const whitelist = ['http://localhost:3000/'] // add your safe domain
+// const corsOptions: CorsOptions = {
+// 	origin: function (origin, callback) {
+// 		if (whitelist.indexOf(origin as string) !== -1 || !origin) {
+// 			callback(null, true)
+// 		} else {
+// 			callback(new Error('Not allowed by CORS'))
+// 		}
+// 	}
+// }
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (_req: Request, res: Response) => {
+	res.send('Welcome!')
+})
+
+// import routes from handlers
+// productRoutes(app)
+// customerRoutes(app)
+// userRoutes(app)
+// orderRoutes(app)
+
+app.listen(port, () => {
+	console.log(`Server listening on port: ${port}`)
+})
