28e2d5f8aa327f8626a5aff1af42cbf71a2bed5f
“[Matti <[matti.tamminen@codecontrol.fi]>
2021-10-07 14:35:51 +0300

    cors

--- storeBack/src/server.ts
+++ storeBack/src/server.ts
@@ -1,18 +1,18 @@
 import express, { Response, Request } from 'express'
 import { config } from './config/config'
-// import cors from 'cors'
+import cors, { CorsOptions } from 'cors'
 import { productRoutes } from './handlers/productsHandler'
 
-// const whitelist = ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:9876'] // add your safe domain
-// const corsOptions: CorsOptions = {
-// 	origin: function (origin, callback) {
-// 		if (whitelist.indexOf(origin as string) !== -1 || !origin) {
-// 			callback(null, true)
-// 		} else {
-// 			callback(new Error('Not allowed by CORS'))
-// 		}
-// 	}
-// }
+const whitelist = ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5432', 'http://storeapi-env.eba-ub3a4zsk.eu-north-1.elasticbeanstalk.com'] // add your safe domain
+const corsOptions: CorsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin as string) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
 
 const app = express()
 const port = config.port | 5432
@@ -22,7 +22,7 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (_req: Request, res: Response) => {
+app.get('/', cors(corsOptions), (_req: Request, res: Response) => {
 	res.send('Connection OK')
 })
 
