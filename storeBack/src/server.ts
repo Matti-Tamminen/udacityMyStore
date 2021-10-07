01fe583c9eb2ca9b6f8c2c79fc8c169cd88c6535
“[Matti <[matti.tamminen@codecontrol.fi]>
2021-10-07 15:50:18 +0300

    cors

--- storeBack/src/server.ts
+++ storeBack/src/server.ts
@@ -3,7 +3,7 @@ import { config } from './config/config'
 import cors, { CorsOptions } from 'cors'
 import { productRoutes } from './handlers/productsHandler'
 
-const whitelist = ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5432', 'http://storeapi-env.eba-ub3a4zsk.eu-north-1.elasticbeanstalk.com'] // add your safe domain
+const whitelist = ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5432', 'http://storeapi-env.eba-ub3a4zsk.eu-north-1.elasticbeanstalk.com', 'http://storefront-s3-bucket.s3-website.eu-north-1.amazonaws.com'] // add your safe domain
 const corsOptions: CorsOptions = {
 	origin: function (origin, callback) {
 		if (whitelist.indexOf(origin as string) !== -1 || !origin) {
